426027b3f37d9c6ab465925035582906088424f0
Movindu Jayathilake <147236691+MovinduJay@users.noreply.github.com>
2024-07-15 21:07:04 +0530

    completed the NavBar

--- frontend/src/components/headers/NavBar.jsx
+++ frontend/src/components/headers/NavBar.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 import { ThemeProvider, THEME_ID, createTheme } from '@mui/material/styles';
+import { ListItem } from '@mui/material';
+import {FaBars} from "react-icons/fa"
+import {motion} from "framer-motion"
+
+
+import photoURL from "../../assets/home/girl.jpg"
 
 const navlinks=[
     {name:'Home',route:'/'},
@@ -15,7 +21,7 @@ const theme = createTheme({
             main:"#ff0000",
         },
         secondary:{
-            main:"00ff00"
+            main:"#00ff00"
         },
     },
 });
@@ -31,6 +37,7 @@ const NavBar = () => {
     const[isFixed,setIsFixed]=useState(false)
     const [isDarkMode,setIsDarkMode]=useState(false)
     const [navBg,setNavBg]= useState('bg-[#15151580')
+    const user=true
 
     const toggleMobileMenu=()=>{
         setIsMobileMenuOpen(!isMobileMenuOpen)
@@ -75,18 +82,39 @@ const NavBar = () => {
     }
     },[scrollPosition])
 
+    const handleLogout=()=>{
+        console.log("Logged out")
+    }
+
   return (
-    <nav className=''>
+    <motion.nav
+    initial={{opacity:0}} 
+    animate={{opacity:1}}
+    transition={{duration:0.5}}
+    className={`${isHome ? navBg:"bg-white dark:bg-black backdrop-blur-2xl"}${isFixed ?
+    'static':'fixed'}top-0 transition-colors duration-500 ease-in-out w-full z-10`}>
       <div className='lg:w-[95%] mx-auto sm:px-6 lg:px-6'>
 
         <div className='px-4 py-4 flex items-center justify-between'>
         {/*logo*/ }
-        <div>
-            <h1 className='text-2x1 inline-flex gap-3 items-center font-bold'>FitConnect<img src="/yoga-logo.png" alt=''className='w-8 h-8'/></h1>
-            <p className='font-bold text-[13px] tracking-[8px]'>Quick Explore</p>
+        <div onClick={()=>navigate('/')} className='flex-shrink-0 cursor-pointer pl-7 md:p-0 flex 
+        items-center'>
+           <div>
+           <h1 className='text-2x1 inline-flex gap-3 items-center font-bold'>
+                FitConnect<img src="/yoga-logo.png" alt=''className='w-8 h-8'/>
+            </h1>
+            <p className='font-bold text-[13px] tracking-[8px]'>Quick Explore
+            </p>
+           </div>
         </div>
 
         {/* mobile menu icons */}
+        <div className='md:hidden flex items-center'>
+            <button type='button' onClick={toggleMobileMenu} className='text-gray-300 hover:text-white focus:outline-none'>
+                <FaBars className="h-6 w-6 hover:text-primary"/>
+            </button>
+        </div>
+
          {/* Navigational Links */}
          <div className='hidden md:block text-black dark:text-white'>
             <div className='flex'>
@@ -94,29 +122,103 @@ const NavBar = () => {
                     {navlinks.map((Link)=>(
                             <li key={Link.route}>
                                 <NavLink 
-                                to={Link.route} 
+                                to={Link.route}
+                                style={{whiteSpace:"nowrap"}} 
                                 className={({isActive})=>
-                                `font-bold ${isActive ? 'text-secondary': `${navBg.includes('bg-transparent') ?
-                                'text-white': 'text-black dark: text-white'}`}hover:text-secondary duration-300`
-                            }>
+                                `font-bold ${
+                                isActive
+                                ? 'text-secondary'
+                                : `${
+                                navBg.includes('bg-transparent') 
+                                ?'text-white'
+                                :'text-black dark: text-white'
+                                }`
+                            }hover:text-secondary duration-300`
+                            }
+                            >
                                     {Link.name}
                                     </NavLink> 
                             </li>
-                        ))}
+                        ))}                        
 
                         {/*Based on users */}
-                       <li> <NavLink to="/login"  className={({isActive})=>
-                                `font-bold ${isActive ? 'text-secondary': `${navBg.includes('bg-transparent') ?
-                                'text-white': 'text-black dark: text-white'}`}hover:text-secondary duration-300`
-                            } >Login</NavLink></li>
-                            {/*color toglle*/}
-                            <li>Light / Dark</li>
+                        {user?null:isLogin?(   
+                        <li> 
+                            <NavLink 
+                            to="/register"  
+                            className={({isActive})=>
+                                `font-bold ${
+                                isActive 
+                                ? 'text-secondary'
+                                : `${
+                                navBg.includes('bg-transparent') 
+                                ?'text-black'
+                                : 'text-black dark: text-white'
+                                }`
+                            }hover:text-secondary duration-300`
+                            } >
+                                Register
+                                </NavLink>
+                                </li>
+                            ):(
+                            <li> 
+                            <NavLink 
+                            to="/login"  
+                            className={({isActive})=>
+                                `font-bold ${
+                                isActive 
+                                ? 'text-secondary'
+                                : `${
+                                navBg.includes('bg-transparent') 
+                                ?'text-black'
+                                : 'text-black dark: text-white'}
+                                `}hover:text-secondary duration-300`
+                            } >Login
+                            </NavLink>
+                            </li>
+                       ) }
+
+                       {
+                        user && <li>
+                            <NavLink to='/dashboard'  className={({isActive})=>
+                                `font-bold ${
+                                isActive 
+                                ? 'text-secondary'
+                                : `${
+                                navBg.includes('bg-transparent') 
+                                ?'text-black'
+                                : 'text-black dark: text-white'
+                                }`
+                            }hover:text-secondary duration-300`
+                            }>Dashboard </NavLink>
+                        </li>
+                       }
+
+                       {
+                        user && <li>
+                            <img src={photoURL} alt='' className='h-[40px] rounded-full w-[40px]'/>
+                        </li>
+                       }
+
+                       {
+                        user && <li ><NavLink onClick={handleLogout} className={'font-bold px-3 py-2 bg-secondary text-white rounded-xl'}>Logout</NavLink></li>
+                       }
+              
+                            {/*color toglle*/} 
+                            <li>
+                            <ThemeProvider theme={theme}>
+                                <div className='flex flex-col justify-center items-center'>
+                                    <switch onChange={()=>setIsDarkMode(!isDarkMode)}/>
+                                        <h1 className='text-[8px]'></h1>
+                                </div>
+                            </ThemeProvider>
+                            </li>
                 </ul>
             </div>
          </div>
       </div>
       </div>
-    </nav>
+    </motion.nav>
   )
 }
 
---
+++ frontend/src/pages/Home/Hero/HeroContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HeroContainer
