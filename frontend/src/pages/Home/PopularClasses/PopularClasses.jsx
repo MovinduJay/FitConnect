e5ceb2f76cf639f386b75fc4bdd5b2c2bde37314
Movindu Jayathilake <147236691+MovinduJay@users.noreply.github.com>
2024-07-19 20:30:18 +0530

    Implemented Popular Classes cards

--- frontend/src/pages/Home/PopularClasses/PopularClasses.jsx
+++ frontend/src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useAxiosFetch from '../../../hooks/useAxiosFetch'
+import Card from './Card';
 
 const PopularClasses = () => {
+  const axiosfetch =useAxiosFetch();
+  const [classes,setClasses]= useState([])
+  useEffect(()=>{
+    const fetchClasses= async()=>{
+      const response =await axiosfetch.get('/classes')
+      //console.log(response.data)
+      setClasses(response.data)
+    }
+
+    fetchClasses()
+  },[])
+
   return (
     <div className='md:w-[80%] mx-auto my-36'>
         <div>
@@ -9,6 +23,12 @@ const PopularClasses = () => {
             <div className='w-[40%] text-center mx-auto my-4'>
                 <p className='text-gray-500'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore fugit beatae nam quis alias.</p>
             </div>
+        </div>
+
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+          {
+            classes.slice(0,6).map((item,index)=><Card key={index} item={item}/>)
+          }
         </div>
       
     </div>
